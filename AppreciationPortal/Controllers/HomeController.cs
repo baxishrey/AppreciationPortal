4ba67395288a364e9af7dcf250cdf681ef9dd8ac
Shrey Baxi <baxishrey@gmail.com>
2017-09-05 21:15:43 +0530

    Initial commit with slideshow

--- AppreciationPortal/Controllers/HomeController.cs
+++ AppreciationPortal/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace AppreciationPortal.Controllers
 {
@@ -24,6 +20,11 @@ namespace AppreciationPortal.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Add()
+        {
             return View();
         }
     }
